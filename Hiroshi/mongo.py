f8ced065f0e0e40ae0ed480777b9a56d77f2e56f
HIROSHI <90719292+UserbotMaps@users.noreply.github.com>
2022-02-25 07:01:56 +0700

    Update mongo.py

--- Hiroshi/mongo.py
+++ Hiroshi/mongo.py
@@ -3,8 +3,8 @@ import sys
 from motor import motor_asyncio
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from GreyCilik import MONGO_DB_URI
-from GreyCilik.confing import get_int_key, get_str_key
+from Hiroshi import MONGO_DB_URI
+from Hiroshi.confing import get_int_key, get_str_key
 
 
 MONGO_PORT = get_int_key("27017")
@@ -16,7 +16,7 @@ client = MongoClient()
 client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
-db = client["GreyCilik"]
+db = client["Hiroshi"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
