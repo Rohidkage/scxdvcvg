f57d9b00328da9c8f8b59d29108990a4358fbef1
HIROSHI <90719292+UserbotMaps@users.noreply.github.com>
2022-03-28 02:18:46 +0700

    Update myanimelist.py

--- Hiroshi/modules/myanimelist.py
+++ Hiroshi/modules/myanimelist.py
@@ -12,7 +12,7 @@ from telegram import (
 )
 from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
 
-from GreyCilik import dispatcher
+from Hiroshi import dispatcher
 
 jikan = Jikan()
 
