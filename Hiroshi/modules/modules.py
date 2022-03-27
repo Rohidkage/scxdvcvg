bbb9c4a4b0ab3cd2e0c9c1e31f39aabcc03ecb26
HIROSHI <90719292+UserbotMaps@users.noreply.github.com>
2022-03-28 02:14:56 +0700

    Update modules.py

--- Hiroshi/modules/modules.py
+++ Hiroshi/modules/modules.py
@@ -1,8 +1,8 @@
 import importlib
 import collections
 
-from GreyCilik import dispatcher, telethn
-from GreyCilik.__main__ import (
+from Hiroshi import dispatcher, telethn
+from Hiroshi.__main__ import (
     CHAT_SETTINGS,
     DATA_EXPORT,
     DATA_IMPORT,
@@ -13,7 +13,7 @@ from GreyCilik.__main__ import (
     USER_INFO,
     USER_SETTINGS,
 )
-from GreyCilik.modules.helper_funcs.chat_status import dev_plus, sudo_plus
+from Hiroshi.modules.helper_funcs.chat_status import dev_plus, sudo_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler
 
