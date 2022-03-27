f42dc37a9d09858901b95a730dca1f70d6cce179
HIROSHI <90719292+UserbotMaps@users.noreply.github.com>
2022-03-28 02:10:04 +0700

    Update memify.py

--- Hiroshi/modules/memify.py
+++ Hiroshi/modules/memify.py
@@ -1,8 +1,8 @@
 import textwrap
 import os
 from PIL import Image, ImageFont, ImageDraw
-from GreyCilik.events import register
-from GreyCilik import LOGGER, TEMP_DOWNLOAD_DIRECTORY, telethn as bot
+from Hiroshi.events import register
+from Hiroshi import LOGGER, TEMP_DOWNLOAD_DIRECTORY, telethn as bot
 
 
 @register(pattern="^/mmf ?(.*)")
@@ -39,7 +39,7 @@ async def drawText(image_path, text):
     if os.name == "nt":
         fnt = "ariel.ttf"
     else:
-        fnt = "./GreyCilik/resources/default.ttf"
+        fnt = "./Hiroshi/resources/default.ttf"
     m_font = ImageFont.truetype(fnt, int((70 / 640) * i_width))
     if ";" in text:
         upper_text, lower_text = text.split(";")
