9678b37cc0fc135b61c5835b43074897a9c1c9ca
HIROSHI <90719292+UserbotMaps@users.noreply.github.com>
2022-03-27 23:02:29 +0700

    Update alive.py

--- Hiroshi/modules/alive.py
+++ Hiroshi/modules/alive.py
@@ -5,20 +5,20 @@ from telethon import events, Button
 from telegram import __version__ as telever
 from telethon import __version__ as tlhver
 from pyrogram import __version__ as pyrover
-from GreyCilik.events import register
-from GreyCilik import telethn as tbot
+from Hiroshi.events import register
+from Hiroshi import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/760e7c0afaf6ba3df8ce7.jpg"
+PHOTO = "https://telegra.ph/file/b3329dd79719e54ba334f.mp4"
 
 @register(pattern=("/alive"))
 async def awake(event):
-  GREY = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Grey Cilik.** \n\n"
+  GREY = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Turbo Robot.** \n\n"
   GREY += "⚪ **I'm Working Properly** \n\n"
-  GREY += f"⚪ **My Master : [Grey](https://t.me/greyyvbss)** \n\n"
+  GREY += f"⚪ **My Master : [Hiro](https://t.me/Bisuinhiro)** \n\n"
   GREY += f"⚪ **Library Version :** `{telever}` \n\n"
   GREY += f"⚪ **Telethon Version :** `{tlhver}` \n\n"
   GREY += f"⚪ **Pyrogram Version :** `{pyrover}` \n\n"
   GREY += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("ʜᴇʟᴘ​", "https://t.me/GreyCilik_bot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/CilikSupport")]]
-  await tbot.send_file(event.chat_id, PHOTO, caption=GREY,  buttons=BUTTON)
+  BUTTON = [[Button.url("ʜᴇʟᴘ​", "https://t.me/TurboHiroBot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/hiroshisupport")]]
+  await tbot.send_file(event.chat_id, PHOTO, caption=HIROSHI,  buttons=BUTTON)
