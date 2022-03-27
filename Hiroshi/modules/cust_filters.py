689b9d357ddc4d27e4b81c6aa1afdcc24712f330
HIROSHI <90719292+UserbotMaps@users.noreply.github.com>
2022-03-28 00:09:49 +0700

    Update cust_filters.py

--- Hiroshi/modules/cust_filters.py
+++ Hiroshi/modules/cust_filters.py
@@ -13,23 +13,23 @@ from telegram.ext import (
     Filters,
 )
 from telegram.utils.helpers import mention_html, escape_markdown
-from GreyCilik import dispatcher, LOGGER, DRAGONS
-from GreyCilik.modules.disable import DisableAbleCommandHandler
-from GreyCilik.modules.helper_funcs.handlers import MessageHandlerChecker
-from GreyCilik.modules.helper_funcs.chat_status import user_admin
-from GreyCilik.modules.helper_funcs.extraction import extract_text
-from GreyCilik.modules.helper_funcs.filters import CustomFilters
-from GreyCilik.modules.helper_funcs.misc import build_keyboard_parser
-from GreyCilik.modules.helper_funcs.msg_types import get_filter_type
-from GreyCilik.modules.helper_funcs.string_handling import (
+from Hiroshi import dispatcher, LOGGER, DRAGONS
+from Hiroshi.modules.disable import DisableAbleCommandHandler
+from Hiroshi.modules.helper_funcs.handlers import MessageHandlerChecker
+from Hiroshi.modules.helper_funcs.chat_status import user_admin
+from Hiroshi.modules.helper_funcs.extraction import extract_text
+from Hiroshi.modules.helper_funcs.filters import CustomFilters
+from Hiroshi.modules.helper_funcs.misc import build_keyboard_parser
+from Hiroshi.modules.helper_funcs.msg_types import get_filter_type
+from Hiroshi.modules.helper_funcs.string_handling import (
     split_quotes,
     button_markdown_parser,
     escape_invalid_curly_brackets,
     markdown_to_html,
 )
-from GreyCilik.modules.sql import cust_filters_sql as sql
-from GreyCilik.modules.connection import connected
-from GreyCilik.modules.helper_funcs.alternate import send_message, typing_action
+from Hiroshi.modules.sql import cust_filters_sql as sql
+from Hiroshi.modules.connection import connected
+from Hiroshi.modules.helper_funcs.alternate import send_message, typing_action
 
 HANDLER_GROUP = 10
 
@@ -607,9 +607,9 @@ def __chat_settings__(chat_id, user_id):
 
 
 __help__ = """
-❂ /filters*:* List all active filters saved in the chat.
+✮ /filters*:* List all active filters saved in the chat.
 *Admin only:*
-❂ /filter <keyword> <reply message>*:* Add a filter to this chat. The bot will now reply that message whenever 'keyword'\
+✮ /filter <keyword> <reply message>*:* Add a filter to this chat. The bot will now reply that message whenever 'keyword'\
 is mentioned. If you reply to a sticker with a keyword, the bot will reply with that sticker. NOTE: all filter \
 keywords are in lowercase. If you want your keyword to be a sentence, use quotes. eg: /filter "hey there" How you \
 doin?
@@ -622,11 +622,11 @@ doin?
  %%%
  Reply 3`
 
-❂ /stop <filter keyword>*:* Stop that filter.
+✮ /stop <filter keyword>*:* Stop that filter.
 
 *Chat creator only:*
 
-❂ /removeallfilters*:* Remove all chat filters at once.
+✮ /removeallfilters*:* Remove all chat filters at once.
 *Note*: Filters also support markdown formatters like: {first}, {last} etc.. and buttons.
 Check /markdownhelp to know more!
 """
