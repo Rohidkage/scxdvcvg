8b1dae8869d5205af57b9700b9e65f46d175578c
HIROSHI <90719292+UserbotMaps@users.noreply.github.com>
2022-03-27 19:47:24 +0700

    Update weather.py

--- Hiroshi/modules/weather.py
+++ Hiroshi/modules/weather.py
@@ -8,10 +8,10 @@ from telethon.tl import functions
 from telethon.tl import types
 from telethon.tl.types import *
 
-from GreyCilik import *
+from Hiroshi import *
 
-from GreyCilik import telethn as tbot
-from GreyCilik.events import register
+from Hiroshi import telethn as tbot
+from Hiroshi.events import register
 
 
 async def is_register_admin(chat, user):
