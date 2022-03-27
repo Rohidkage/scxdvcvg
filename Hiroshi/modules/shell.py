1b4d0c6492f9023a9f90d39d9bae387a9f41b780
HIROSHI <90719292+UserbotMaps@users.noreply.github.com>
2022-03-27 21:17:57 +0700

    Update shell.py

--- Hiroshi/modules/shell.py
+++ Hiroshi/modules/shell.py
@@ -1,7 +1,7 @@
 import subprocess
 
-from GreyCilik import LOGGER, dispatcher
-from GreyCilik.modules.helper_funcs.chat_status import dev_plus
+from Hiroshi import LOGGER, dispatcher
+from Hiroshi.modules.helper_funcs.chat_status import dev_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler
 from telegram.ext.dispatcher import run_async
