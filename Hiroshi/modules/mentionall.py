a903821103e006ef59a79562d521ab96d9969565
HIROSHI <90719292+UserbotMaps@users.noreply.github.com>
2022-03-28 02:10:58 +0700

    Update mentionall.py

--- Hiroshi/modules/mentionall.py
+++ Hiroshi/modules/mentionall.py
@@ -7,7 +7,7 @@ from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
 from telethon.tl.functions.channels import GetParticipantRequest
 from telethon.errors import UserNotParticipantError
 
-from GreyCilik import telethn as Client
+from Hiroshi import telethn as Client
 
 spam_chats = []
 
