26c4db1dd220df3e47feef840283816349e45404
HIROSHI <90719292+UserbotMaps@users.noreply.github.com>
2022-03-28 03:11:54 +0700

    Update nsfw_sql.py

--- Hiroshi/modules/sql/nsfw_sql.py
+++ Hiroshi/modules/sql/nsfw_sql.py
@@ -1,6 +1,6 @@
 import threading
 from sqlalchemy import Column, String
-from GreyCilik.modules.sql import BASE, SESSION
+from Hiroshi.modules.sql import BASE, SESSION
 
 #   |----------------------------------|
 #   |  Test Module by @EverythingSuckz |
