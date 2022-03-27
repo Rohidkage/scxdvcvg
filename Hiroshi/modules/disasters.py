7d3232e52b0cb3df769f94f5bef7db07ebef4b8d
HIROSHI <90719292+UserbotMaps@users.noreply.github.com>
2022-03-28 00:22:24 +0700

    Update disasters.py

--- Hiroshi/modules/disasters.py
+++ Hiroshi/modules/disasters.py
@@ -3,7 +3,7 @@ import json
 import os
 from typing import Optional
 
-from GreyCilik import (
+from Hiroshi import (
     DEV_USERS,
     OWNER_ID,
     DRAGONS,
@@ -13,13 +13,13 @@ from GreyCilik import (
     WOLVES,
     dispatcher,
 )
-from GreyCilik.modules.helper_funcs.chat_status import (
+from Hiroshi.modules.helper_funcs.chat_status import (
     dev_plus,
     sudo_plus,
     whitelist_plus,
 )
-from GreyCilik.modules.helper_funcs.extraction import extract_user
-from GreyCilik.modules.log_channel import gloggable
+from Hiroshi.modules.helper_funcs.extraction import extract_user
+from Hiroshi.modules.log_channel import gloggable
 from telegram import ParseMode, TelegramError, Update
 from telegram.ext import CallbackContext, CommandHandler
 from telegram.utils.helpers import mention_html
