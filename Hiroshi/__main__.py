bf849ba34c6478d9fe43145bcab0a1de63b43e1a
HIROSHI <90719292+UserbotMaps@users.noreply.github.com>
2022-03-31 06:11:05 +0700

    Update __main__.py

--- Hiroshi/__main__.py
+++ Hiroshi/__main__.py
@@ -53,6 +53,7 @@ from telegram.ext import (
 )
 from telegram.ext.dispatcher import DispatcherHandlerStop, run_async
 from telegram.utils.helpers import escape_markdown
+from Hiroshi.modules.language import gs
 
 
 def get_readable_time(seconds: int) -> str:
