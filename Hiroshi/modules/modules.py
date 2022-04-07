332f6f581c2ee56c1975daf7e941ff60610237da
zeinzo <91390078+zeinzo@users.noreply.github.com>
2022-04-08 04:22:11 +0700

    Update modules.py

--- Hiroshi/modules/modules.py
+++ Hiroshi/modules/modules.py
@@ -28,7 +28,7 @@ def load(update: Update, context: CallbackContext):
     )
 
     try:
-        imported_module = importlib.import_module("GreyCilik.modules." + text)
+        imported_module = importlib.import_module("Hiroshi.modules." + text)
     except:
         load_messasge.edit_text("Does that module even exist?")
         return
@@ -99,7 +99,7 @@ def unload(update: Update, context: CallbackContext):
     )
 
     try:
-        imported_module = importlib.import_module("GreyCilik.modules." + text)
+        imported_module = importlib.import_module("Hiroshi.modules." + text)
     except:
         unload_messasge.edit_text("Does that module even exist?")
         return
