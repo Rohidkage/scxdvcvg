f5a0efc2a8fc83f4716e6c6d0ae1f54f78b810e7
zeinzo <91390078+zeinzo@users.noreply.github.com>
2022-04-07 10:12:13 +0700

    Update __main__.py

--- Hiroshi/__main__.py
+++ Hiroshi/__main__.py
@@ -222,7 +222,7 @@ def start(update: Update, context: CallbackContext):
         else:
             first_name = update.effective_user.first_name
             update.effective_message.reply_text(
-                PM_START_TEXT.format(
+                text=gs(chat.id, "pm_start_text").format(
                     escape_markdown(first_name),
                     escape_markdown(uptime),
                     sql.num_users(),
