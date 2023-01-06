3a3397e0d7014ec473b747616139bc8167b17e3d
CuteNeehh <105994116+Neehh@users.noreply.github.com>
2023-01-06 20:39:09 +0700

    Update alive.py

--- Hiroshi/modules/alive.py
+++ Hiroshi/modules/alive.py
@@ -13,12 +13,12 @@ PHOTO = "https://telegra.ph/file/b3329dd79719e54ba334f.mp4"
 
 @register(pattern=("/alive"))
 async def awake(event):
-  GREY = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Turbo Robot.** \n\n"
+  GREY = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Hiroshi Robot.** \n\n"
   GREY += "⚪ **I'm Working Properly** \n\n"
-  GREY += f"⚪ **My Master : [Hiro](https://t.me/Bisuinhiro)** \n\n"
+  GREY += f"⚪ **My Master : [Hiroshi](https://t.me/splesneey)** \n\n"
   GREY += f"⚪ **Library Version :** `{telever}` \n\n"
   GREY += f"⚪ **Telethon Version :** `{tlhver}` \n\n"
   GREY += f"⚪ **Pyrogram Version :** `{pyrover}` \n\n"
   GREY += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("ʜᴇʟᴘ​", "https://t.me/TurboHiroBot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/hiroshisupport")]]
+  BUTTON = [[Button.url("ʜᴇʟᴘ​", "https://t.me/hiroshirobot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/hiroxsupport")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=HIROSHI,  buttons=BUTTON)
