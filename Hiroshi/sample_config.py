5ff7190b83a30273059b36cccf85a8e9d6be1af3
CuteNeehh <105994116+Neehh@users.noreply.github.com>
2023-01-06 18:24:12 +0700

    Update sample_config.py

--- Hiroshi/sample_config.py
+++ Hiroshi/sample_config.py
@@ -17,14 +17,14 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 1821140802  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Bisubiarenak"
-    SUPPORT_CHAT = "hiroshisupport"  # Your own group for support, do not add the @
+    OWNER_ID = 314180533  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "splesneey"
+    SUPPORT_CHAT = "hiroxsupport"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001676791593
+        -1001840094518
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001676791593
+        -1001840094518
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
