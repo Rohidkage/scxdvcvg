be8fa71876973e87f65cec5d8522a5349292efdc
khansa gunawan <112273130+failedc0de@users.noreply.github.com>
2023-01-08 16:24:25 +0700

    Update __main__.py

--- Hiroshi/__main__.py
+++ Hiroshi/__main__.py
@@ -224,7 +224,7 @@ def start(update: Update, context: CallbackContext):
         else:
             first_name = update.effective_user.first_name
             update.effective_message.reply_photo(
-                photo=PRIME_IMG
+                photo=PRIME_IMG,
                 caption=PM_START_TEXT.format(
                     escape_markdown(first_name),
                     escape_markdown(uptime),
